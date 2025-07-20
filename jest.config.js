87e08da4aea6beaf3989f40b4088b4b4cbcb6cb7
flyjeray <flyjeray@gmail.com>
2025-07-20 14:38:34 +0300

    implement coverage testing - new Text component not Tested on purpose

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,18 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/components/**/*.{js,jsx,ts,tsx}",
+    "!src/components/**/*.d.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
