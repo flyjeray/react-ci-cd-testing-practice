cbc863bd94fb3e1c1ccd87ab9ea401df2064b4d4
flyjeray <flyjeray@gmail.com>
2025-07-20 15:42:26 +0300

    add some components to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Text } from "./components/Text";
+import CheckboxWithLabel from "./components/CheckboxWithLabel";
 
 function App() {
   return (
     <div className="App">
       <h1>It works</h1>
+      <Text value={"Some text"} />
+      <CheckboxWithLabel labelOn={"On"} labelOff={"Off"} />
     </div>
   );
 }
